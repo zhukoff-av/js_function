ff3ff26b15ce4075b1d2dd73b42987c56505a305
zhukoff-av <mr.zhukoff@gmail.com>
2018-01-16 05:25:53 +0300

    refactoring makeRectangle

--- src/makeRectangle.js
+++ src/makeRectangle.js
@@ -1,17 +1,15 @@
 export const makeRectangle = (point, width, height) => cons(point, cons(width, height));
 
 export const startPoint = rectangle => car(rectangle);
-
-export const width = rectangle => car(cdr(rectangle));
 export const height = rectangle => cdr(cdr(rectangle));
+export const width = rectangle => car(cdr(rectangle));
 
-export const square = rectangle => width(rectangle) * height(rectangle);
-export const perimeter = rectangle => 2 * (width(rectangle) + height(rectangle));
+export const square = rectangle => height(rectangle) * width(rectangle);
+export const perimeter = rectangle => 2 * (height(rectangle) + width(rectangle));
 
-export const containsTheOrigin = rectangle => {
-    const leftTopPoint = startPoint(rectangle);
-    const leftBottomPoint =  makePoint(getX(leftTopPoint), getY(leftTopPoint) - height(rectangle));
-    const rightBottomPoint =  makePoint(getX(leftTopPoint) + width(rectangle), getY(leftTopPoint) - height(rectangle));
-    const rightTopPoint = makePoint(getX(leftTopPoint) + width(rectangle), getY(leftTopPoint) - height(rectangle));
-    return quadrant(leftTopPoint) === 2 && quadrant(leftBottomPoint) === 3 && quadrant(rightBottomPoint) === 4;
+export const containsTheOrigin = (rectangle) => {
+  const point1 = startPoint(rectangle);
+  const point2 = makePoint(getX(point1) + width(rectangle), getY(point1) - height(rectangle));
+
+  return quadrant(point1) === 2 && quadrant(point2) === 4;
 };
