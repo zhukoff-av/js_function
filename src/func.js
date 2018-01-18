aac7d65d4e151b156de2623f0cce8b3f1559326d
zhukoff-av <mr.zhukoff@gmail.com>
2018-01-18 22:29:05 +0300

    function's Sum of squares, Square of sum

--- src/func.js
+++ src/func.js
@@ -13,3 +13,30 @@ const isPowerOfThree = (num) => {
 };
 
 export default isPowerOfThree;
+
+// Sum of squares
+const sumOfSquares = (n) => {
+  let sum = 0;
+  let i = 1;
+  while (i <= n) {
+    sum += i * i;
+    i += 1;
+  }
+
+  return sum;
+};
+
+// Square of sum
+const squareOfSum = (n) => {
+  let sum = 0;
+  let i = 1;
+  while (i <= n) {
+    sum += i;
+    i += 1;
+  }
+
+  return sum * sum;
+};
+
+export const sumSquareDifference = n =>
+  squareOfSum(n) - sumOfSquares(n);
