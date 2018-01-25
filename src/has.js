1c617f0a1d674abf927aefebc4141919172dda15
Zhukov <anton.zhukov@t-systems.ru>
2018-01-25 13:32:27 +0300

    function has

---
+++ src/has.js
@@ -0,0 +1,27 @@
+//TODO need to import { l, cons, head, tail, isEmpty, toString as listToString }
+
+export const has = (list, element) => {
+  if (isEmpty(list)) {
+    return false;
+  }
+  if (head(list) === element) {
+    return true;
+  }
+
+  return has(tail(list), element);
+};
+
+export const reverse = (list) => {
+  const iter = (items, acc) =>
+    (isEmpty(items) ? acc : iter(tail(items), cons(head(items), acc)));
+
+  return iter(list, l());
+};
+
+export const concat = (list1, list2) => {
+  if (isEmpty(list1)) {
+    return list2;
+  }
+
+  return cons(head(list1), concat(tail(list1), list2));
+};
