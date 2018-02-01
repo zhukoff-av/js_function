16adbfae60b74c9a039d9d5f42f3ec53532187d9
Anton Zhukov <mr.zhukoff@gmail.com>
2018-02-01 19:07:48 +0300

    add reduce

---
+++ src/reduce.js
@@ -0,0 +1,13 @@
+export const reduce = (func, acc, elements) => {
+  if (isEmpty(elements)) {
+    return acc;
+  }
+
+  return reduce(func, func(head(elements), acc), tail(elements));
+};
+
+export const emptyTagsCount = (tagName, elements) => {
+  const predicate = element => is(tagName, element) && value(element) === '';
+  const func = (element, acc) => (predicate(element) ? acc + 1 : acc);
+  return reduce(func, 0, elements);
+};
