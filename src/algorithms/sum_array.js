150ed3e81f6c6da8fa2cc6696fa6fe9ff3e1d5c2
zhukoff-av <mr.zhukoff@gmail.com>
2018-03-10 20:41:09 +0300

    add sumList

--- src/algorithms/sum_array.js
+++ src/algorithms/sum_array.js
@@ -1,4 +1,5 @@
-const sum = (arr) => {
+// SUM ARRAY
+const sumArray = (arr) => {
   let total = 0;
   for (let x = 0; x < arr.length; x += 1) {
     total += arr[x];
@@ -6,4 +7,13 @@ const sum = (arr) => {
   return total;
 };
 
-console.log(sum([1, 2, 3, 4])); // 10
+console.log(sumArray([1, 2, 3, 4])); // 10
+
+const sumList = (list) => {
+  if (list.length === 0) {
+    return 0;
+  }
+  return list[0] + sumList(list.slice(1));
+};
+
+console.log(sumList([1, 2, 3, 4])); // 10
