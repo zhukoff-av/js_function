9a37ae59d031434575724f5a868226ccaea547f5
zhukoff-av <mr.zhukoff@gmail.com>
2018-03-20 15:25:02 +0300

    map fn

---
+++ src/funFunFunction/map.js
@@ -0,0 +1,27 @@
+// create an object
+
+const myTaskForToday = [
+    {
+    'name': 'first',
+    'description': 'it is first task for today',
+    'duration': 1,
+    'durationType': 'h'
+    },
+    {
+    'name': 'second',
+    'description': 'it is second task for today',
+    'duration': 2,
+    'durationType': 'h'
+    },
+    {
+    'name': 'third',
+    'description': 'it is third task for today',
+    'duration': 3,
+    'durationType': 'm'
+    },
+    ];
+  
+    // using Map for dispay
+  const nameTaskForToday = myTaskForToday.map((task) => task.name);
+  
+  console.log(nameTaskForToday); // ["first", "second", "third"]
