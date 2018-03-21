62a61448fddbde77b68c85b00c8ee9e317f40cc7
zhukoff-av <mr.zhukoff@gmail.com>
2018-03-21 14:43:14 +0300

    add unique v / collections

--- src/collection.js
+++ src/collection.js
@@ -7,8 +7,28 @@ const uniq = arr => {
     }
     return acc;
   }, []);
-  console.log(result); //[1,2,3,4,5]
+  // console.log(result); //[1,2,3,4,5]
 }
-
-//uniq([1, 2, 1, 2, 3, 5, 4, 5, 3, 4, 4, 4, 4]);
+// console.log('Remove duplicate items in array: ');
+uniq([1, 2, 1, 2, 3, 5, 4, 5, 3, 4, 4, 4, 4]);
 uniq([-2, 20, 0, 4, 20, 0]);
+
+// unique value
+let uniq1 =  coll => coll.reduce((acc, value) =>
+  (acc.includes(value) ? acc : acc.concat(value)), []);
+
+const un = ([-2, 20, 0, 4, 20, 0]);
+
+console.log(uniq1(un));
+
+// Reduce / reduceRight
+const planets = ['Меркурий', 'Венера', 'Земля', 'Марс'];
+const closer = planets.reduceRight((acc, planet) => `${acc} ${planet}`, 'всё ближе к Солнцу:'); // 'всё ближе к Солнцу: Марс   Земля Венера Меркурий'
+const further = planets.reduce((acc, planet) => `${acc} ${planet}`);
+
+// console.log(further);
+// console.log(closer);
+
+const planets1 = 'Mercury,Venus,Earth,Mars';
+// console.log(planets1.split(',')); // [ 'Mercury', 'Venus', 'Earth', 'Mars' ]
+// console.log(planets1.split(',').join('-')); // 'Mercury-Venus-Earth-Mars'
